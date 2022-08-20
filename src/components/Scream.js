7a5bdab90574b2b77b9d293ca0c7e6b6e4df036f
JesusRules <36013969+JesusRules@users.noreply.github.com>
2022-08-19 22:45:21 -0400

    Lower version of react router

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
-import {withStyles, Typography } from '@mui/material';
+import {Typography } from '@mui/material';
+// import withStyles from '@mui/material';
+import {Link} from 'react-router-dom'
 
 // MUI Stuff
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
+// const styles = theme => ({
+//     card: {
+//         display: 'flex'
+//     }
+// });
+
 const styles = {
     card: {
-        display: 'flex'
+        display: 'flex',
+        marginBottom: 20,
+    },
+    image:{
+        minWidth: 200,
+    },
+    content:{
+        padding: 25
     }
 }
 
@@ -19,12 +34,12 @@ class Scream extends Component {
     //   const classes = this.props.classes;
     //   const scream = this.props.scream;
     return (
-      <Card>
+      <Card className={classes.card}>
         <CardMedia
         image={userImage}
-        title="Profile image"/>
-        <CardContent>
-            <Typography variant="h5">{userHandle}</Typography>
+        title="Profile image" className={classes.image}/>
+        <CardContent >
+            <Typography variant="h5" component={Link} to={`users/${userHandle}`} color="primary">{userHandle}</Typography>
             <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
             <Typography variant="body1">{body}</Typography>
         </CardContent>
@@ -34,3 +49,4 @@ class Scream extends Component {
 }
 
 export default withStyles(styles)(Scream);
+// export default Scream;
